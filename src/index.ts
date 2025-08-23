4319941105a1b9f989956f37c4a836675f01d44a
mizuki-y <mizuki-y@syati.info>
2025-08-23 23:25:38 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,31 @@ import {
     map,
     rule,
     writeToProfile,
-    ifApp
+    ifApp,
+    ifDevice,
+    ifKeyboardType
 } from 'karabiner.ts'
 import apps from "./apps";
 import emacsRules from "./emacsRules";
 import chromeRules from './chromeRule';
-import {symbolMode, capsToHyperRule} from "./common";
+import { symbolMode, capsToHyperRule } from "./common";
 
 
 writeToProfile('personal', [
     //symbolMode,
     capsToHyperRule,
-    
 
-    rule('[VNC] shortcuts', ifApp([apps.vnc, apps.universalcontrol]).unless()).manipulators([
-        map('spacebar', '⌃⇧').to('spacebar', '⌃⇧')
+
+    rule('[VNC] shortcuts a', ifApp([apps.vnc, apps.universalcontrol])).manipulators([
+        // 内蔵キーボードからの入力は無視（親PCには送らない）
+        map('spacebar', '⌃⇧')
+            .to('vk_none')
+            .condition(ifDevice({ is_built_in_keyboard: true })),
+
+        // 内蔵キーボード以外（Universal Control 由来）はそのまま通す
+        map('spacebar', '⌃⇧')
+            .to('spacebar', '⌃⇧')
+            .condition(ifDevice({ is_built_in_keyboard: false }))
     ]),
 
     rule('[Jetbrains] Basic keys', ifApp(apps.jetbrains)).manipulators([
